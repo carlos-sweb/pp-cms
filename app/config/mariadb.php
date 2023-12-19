52d4fe45733e1a70e94399d0ce5da6ef765c8e99
carlos-sweb <c4rl0sill3sc4@gmail.com>
2023-12-19 20:09:55 -0300

    update

---
+++ app/config/mariadb.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    "DB_USER"=>"",
+
+    "DB_NAME"=>"",
+
+    "DB_PORT"=>"3306",
+
+    "DB_HOST"=>"",
+
+    "DB_PASS"=>""
+    
+];
--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <?php
-require "vendor/autoload.php";
+require __DIR__."/vendor/autoload.php";
+$path_config = __DIR__."/app/config/mariadb.php";
+$config = file_exists($path_config) ? require $path_config : [ "DB_USER"=>"","DB_NAME"=>"","DB_HOST"=>"","DB_PASS"=>"","DB_PORT"=>"3306"];
+
+putenv("DB_NAME=".$config["DB_NAME"]);
+putenv("DB_USER=".$config["DB_USER"]);
+putenv("DB_HOST=".$config["DB_HOST"]);
+putenv("DB_PASS=".$config["DB_PASS"]);
 $f3 = \Base::instance();
+$cache=Cache::instance();
+$sessionCache=new Cache('folder=var/session');        
+$sess=new Session(NULL,"CSRF",$sessionCache);
+$f3->config("config.ini",true);
+$f3->config("routes.ini");
 $options = array(
   \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,// generic attribute
   \PDO::ATTR_PERSISTENT => TRUE,// we want to use persistent connections
   \PDO::MYSQL_ATTR_COMPRESS => TRUE,// MySQL-specific attribute
 );
+
 try{
-    $db = new \DB\SQL('mysql:host=localhost;port=3306;dbname=ppcms','root','c4rl0sill', $options);
+    $db = new \DB\SQL('mysql:host='.$f3->get('db.HOST').';port=3306;dbname='.$f3->get('db.NAME').';charset=utf8',$f3->get('db.USER'),$f3->get('db.PASS'), $options);
 }catch( PDOException $e){
     $f3->set("DB.error.code",$e->getCode());
     $f3->set("DB.error.message",$e->getMessage());            
 }
-$cache=Cache::instance();
-$sessionCache=new Cache('folder=var/session');        
-$sess=new Session(NULL,"CSRF",$sessionCache);
-$f3->config("config.ini");
-$f3->config("routes.ini");
 $f3->run();
 ?>
